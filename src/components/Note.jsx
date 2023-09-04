4aa0d99f579c7f95a5603553e14d7bf237b69b9c
Mehul Jorwekar <84345667+mehul6729@users.noreply.github.com>
2023-09-05 00:27:09 +0530

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>for a title</h1>
+            <p>for the content</p>
+        </div>
+    );
+}
+
+export default Note;
